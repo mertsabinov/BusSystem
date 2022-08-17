6761f125b9cf5210ab51e62b41eee201049d2bb6
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-17 12:25:45 +0200

    add(goTest): Save and Search

--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -6,22 +6,72 @@ import (
 	"testing"
 )
 
+func chackError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func chackDeepEqual(t *testing.T, got bus.Bus, want bus.Bus) {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got = %v want = %v", got, want)
+	}
+}
+
+func TestData_Search(t *testing.T) {
+	d := Data{
+		"1": {
+			Id:             "1",
+			Capacity:       30,
+			EmptySeat:      []int{},
+			DeparturePoint: "test",
+			Destination:    "test test",
+		},
+	}
+
+	b := bus.Bus{
+		Id:             "1",
+		Capacity:       30,
+		EmptySeat:      []int{},
+		DeparturePoint: "test",
+		Destination:    "test test",
+	}
+
+	result, err := d.Search("1")
+	chackError(t, err)
+	got := result
+	want := b
+	chackDeepEqual(t, got, want)
+}
+
 func TestData_Save(t *testing.T) {
 	d := Data{}
 	b := bus.Bus{
-		Id: "1",
-		Capacity: 30,
-		EmptySeat: []int,
+		Id:             "1",
+		Capacity:       30,
+		EmptySeat:      []int{},
 		DeparturePoint: "test",
-		Destination: "test test",
+		Destination:    "test test",
 	}
 
-	d.Save(b,"1")
+	t.Run("Db Save", func(t *testing.T) {
+		err := d.Save(b, b.Id)
+		chackError(t, err)
 
-	got,_ := d.Search(b.Id)
-	want := b
+		result, err := d.Search(b.Id)
+		chackError(t, err)
+		got := result
+		want := b
+		chackDeepEqual(t, got, want)
+	})
 
-	if !reflect.DeepEqual(got,b){
-		t.Errorf("got = %q want = %q",got,want)
-	}
+	t.Run("Db key AlReady Existi", func(t *testing.T) {
+		got := d.Save(b, b.Id)
+		want := DbAlReadyExisti
+		if got != want {
+			t.Errorf("got = %s want = %s", got, want)
+		}
+	})
+
 }
