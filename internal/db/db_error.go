55e57c046badf25e86e297a79f7408989bfb5be6
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-17 12:25:20 +0200

    add(DbError): db error type

--- internal/db/db_error.go
+++ internal/db/db_error.go
@@ -1,1 +1,12 @@
 package db
+
+type DbError string
+
+const (
+	DbKeyNotFound   DbError = "Key not found"
+	DbAlReadyExisti DbError = "Db Key AlReady Exist"
+)
+
+func (d DbError) Error() string {
+	return string(d)
+}
