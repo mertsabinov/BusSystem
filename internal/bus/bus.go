682a37b99e5cac9a1a4b7cb6dbafdbec819fd243
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-19 12:13:10 +0200

    add(bus)

--- internal/bus/bus.go
+++ internal/bus/bus.go
@@ -3,9 +3,24 @@ package bus
 type Bus struct {
 	Id             string
 	Capacity       int
-	EmptySeat      []int
+	EmptySeat      map[int]bool
 	DeparturePoint string
 	Destination    string
+}
+
+func (b Bus) CheckSeatNumber(seatNumber int) error {
+	if seatNumber < 1 {
+		return BusCheckSeatNumberSmall
+	}
+	if seatNumber > b.Capacity {
+		return BusCheckSeatNumberBig
+	}
+	return nil
+}
+
+func (b Bus) SeatSearch(seatNumber int) (Bus, error) {
+
+	return Bus{}, nil
 }
 
 func (b Bus) ReserveSeat(seatNumber int) error {
