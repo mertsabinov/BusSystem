593a6c31456bf29b6e17a8afa021e7240b71cf55
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-19 12:13:03 +0200

    add(busError): bus check error

--- internal/bus/bus_error.go
+++ internal/bus/bus_error.go
@@ -1,1 +1,14 @@
 package bus
+
+type busErr string
+
+const (
+	BusErrSeatFull          busErr = "The entered seat number is full"
+	BusSeatNumberFalse      busErr = "This seat number is false"
+	BusCheckSeatNumberSmall busErr = "this seat number must be biger than 0"
+	BusCheckSeatNumberBig   busErr = "this seat number must be smaller than capacity"
+)
+
+func (e busErr) Error() string {
+	return string(e)
+}
