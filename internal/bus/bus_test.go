15d49a0aae81ffd00ab158a2c09908cbfc6f7410
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-19 14:23:46 +0200

    add(busTest): bus seatSearch

--- internal/bus/bus_test.go
+++ internal/bus/bus_test.go
@@ -8,7 +8,7 @@ import (
 var testBus = Bus{
 	Id:             "1",
 	Capacity:       30,
-	EmptySeat:      map[int]bool{},
+	EmptySeat:      map[int]bool{10: false, 11: true},
 	DeparturePoint: "test",
 	Destination:    "test test",
 }
@@ -50,6 +50,27 @@ func TestBus_checkSeatNumber(t *testing.T) {
 	t.Run("Check seat number small", func(t *testing.T) {
 		got := testBus.CheckSeatNumber(0)
 		want := BusCheckSeatNumberSmall
+		if got != want {
+			t.Errorf("got = %s want = %s", got, want)
+		}
+	})
+}
+
+func TestBus_SeatSearch(t *testing.T) {
+
+	t.Run("Seat Search", func(t *testing.T) {
+		got, err := testBus.SeatSearch(11)
+		checkError(t, err)
+
+		want := true
+		if got != want {
+			t.Errorf("got = %t want = %t", got, want)
+		}
+	})
+
+	t.Run("Seat search error", func(t *testing.T) {
+		_, got := testBus.SeatSearch(1)
+		want := BusSeatNumberFalse
 		if got != want {
 			t.Errorf("got = %s want = %s", got, want)
 		}
