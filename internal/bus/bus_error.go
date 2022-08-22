8535cb2127355e25968e419d1bbf077b2fab147f
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-22 10:14:28 +0200

    add(busError): this seat number is empty

--- internal/bus/bus_error.go
+++ internal/bus/bus_error.go
@@ -3,6 +3,7 @@ package bus
 type BusErr string
 
 const (
+	BusErrSeatNumberEmpty   BusErr = "This seat number is empty"
 	BusErrSAlleatFull       BusErr = "All seats are full"
 	BusSeatNumberFalse      BusErr = "This seat number is false"
 	BusCheckSeatNumberSmall BusErr = "this seat number must be biger than 0"
