0d4d1c4db90819024c50ad9f2069a7d079a5dfb5
mertovich <88425310+mertovich@users.noreply.github.com>
2022-08-22 11:30:09 +0200

    typo(busError): BusError

--- internal/bus/bus_error.go
+++ internal/bus/bus_error.go
@@ -1,16 +1,16 @@
 package bus
 
-type BusErr string
+type BusError string
 
 const (
-	BusErrSeatNumberEmpty   BusErr = "This seat number is empty"
-	BusErrSAlleatFull       BusErr = "All seats are full"
-	BusSeatNumberFalse      BusErr = "This seat number is false"
-	BusCheckSeatNumberSmall BusErr = "this seat number must be biger than 0"
-	BusCheckSeatNumberBig   BusErr = "this seat number must be smaller than capacity"
-	BusSeatNumberFull       BusErr = "This seat number is full"
+	BusErrSeatNumberEmpty   BusError = "This seat number is empty"
+	BusErrSAlleatFull       BusError = "All seats are full"
+	BusSeatNumberFalse      BusError = "This seat number is false"
+	BusCheckSeatNumberSmall BusError = "this seat number must be biger than 0"
+	BusCheckSeatNumberBig   BusError = "this seat number must be smaller than capacity"
+	BusSeatNumberFull       BusError = "This seat number is full"
 )
 
-func (e BusErr) Error() string {
+func (e BusError) Error() string {
 	return string(e)
 }
